f2372aaaa198140a3de020bab1e72f5f14383909
Austin Graves <austingraves106@aol.com>
2019-10-29 10:44:08 -0500

    Added the primary key and some comments to the scripts

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,11 @@ create database burgers_db;
 -- Set this database to priority for use.
 use burgers_db;
 
+-- Creating the table
 create table burgers(
   id int AUTO_INCREMENT not null,
   burger_name varchar(100),
-  devoured boolean default false
+  devoured boolean default false,
+  primary key(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- Inserting into the database
 insert into burgers (burger_name) values ('Test');
 insert into burgers (burger_name, devoured) values ('Hello', true);
 insert into burgers (burger_name, devoured) values ('again', true);
