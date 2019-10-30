5346eedbb2dce0cd55f9657009966022d4d85a9f
Austin Graves <austingraves106@aol.com>
2019-10-30 11:07:22 -0500

    Working on the UI for the application using handelbars. Attempting to wrap up the UI to continue working on the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressHandleBars  = require('express-handlebars');
 const port = process.env.PORT || 3000; 
 const app = express();
+const path = require('path');
  
 app.engine('handlebars', expressHandleBars());
 app.set('view engine', 'handlebars');
@@ -10,6 +11,7 @@ app.use(express.json());
 const routes = require('./controllers/burgers_controller.js');
 
 app.use(routes)
+app.use(express.static(path.join(__dirname, 'public/assets/css')));
 
 app.listen(port, function() {
   console.log(`Server Running On Port: ${port}`);
